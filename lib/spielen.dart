f455ced05f90a86ef9328771bf75ac88b1cbfb46
Hanna <mhannasophie@gmail.com>
2021-08-20 11:39:34 +0200

    erste Anpassungen

---
+++ lib/spielen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PlayPage extends StatelessWidget {
+  PlayPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hier gehts rund!'),
+      ),
+    );
+  }
+}
